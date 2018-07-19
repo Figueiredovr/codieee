897251680ad8315c49017625dcf3294e20be7a61
Heitor Rodrigues <heitor1994.hr@gmail.com>
2018-07-19 17:02:04 -0300

    3 divisible pairs

---
+++ 3_Divisble_Pairs/solve.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[100001];
+
+int main() {
+    int n;
+    scanf ("%d",&n);
+
+    long long ones = 0, twos = 0, zeros = 0;
+
+    for (int i = 0; i < n; i++){
+        scanf ("%d",&arr[i]);
+        arr[i] %= 3;
+        if (arr[i] == 1) ones++;
+        if (arr[i] == 2) twos++;
+        if (arr[i] == 0) zeros++;
+    }
+
+    long long res = ones*twos;
+
+    res += (zeros*(zeros-1))/2;
+
+    printf ("%lld\n",res);
+    return 0;
+}
