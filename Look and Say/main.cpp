d89d4b7f6692ab043a62662cffe27dcfc1f5240a
mclopes1 <cruzlopesmurilo@gmail.com>
2018-07-19 19:29:54 -0300

    Questão look and say. Tipo de questão: String

---
+++ Look and Say/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+string lookandsay(int N){
+    string number = "1";
+    string new_number = "";
+    char aux;
+    int count = 0;
+
+    for (int i = 1; i < n; i++){
+        aux = number[0];
+        for (int j = 0; j < number.size(); j++){
+            if (aux == number[j]){
+                count++;
+            }
+            else{
+                new_number+= to_string(count) + aux;
+                count = 0;
+                aux = number[j];
+                count++;
+            }
+        }
+        new_number+= to_string(count) + aux;
+        count = 0;
+        number = new_number;
+        new_number = "";
+    }
+
+    return number;
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    printf("%s\n", lookandsay(n).c_str());
+}
